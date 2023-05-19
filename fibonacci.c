67a3912f8489c6d726745c73b489f32017fe98f1
venombeast123 <134013130+venombeast123@users.noreply.github.com>
2023-05-19 22:39:37 +0530

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,s=0,p=1,c;
+    scanf("%d\n",&n);
+    while(s<=n)
+    {
+      printf("%d,  ",s);
+      c=s+p;
+      s=p;
+      p=c;
+
+    }
+  return 0;  
+}   }
+        for(j=(i-1)*2-1; j>=1; j-=2)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
